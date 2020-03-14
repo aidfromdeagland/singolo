1938b4bcb4e39a02d69a5ab0b6bf9136b8c40efb
aidfromdeagland <aidfromdeagland@gmail.com>
2020-03-15 00:08:04 +0300

    feat: implement slider and portfolio section interactivity

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-let navItems = document.querySelectorAll('.nav-list__item');
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
-navItems.forEach(x => x.addEventListener('click', (evt) => {
+let navItems = document.querySelectorAll('.nav-list__item');
+let slider = document.querySelector('.slider');
+let sliderControls = slider.querySelectorAll('.slider-control');
+let slides = slider.querySelectorAll('.slide');
+let iphones = document.querySelectorAll('.iphone');
+let filterButtons = document.querySelectorAll('.filter__button');
+let galleryImages = document.querySelectorAll('.gallery__image');
+
+navItems.forEach(x => x.addEventListener('click', () => {
   let currentActive = document.querySelector('.nav-list__item_active');
   currentActive.classList.toggle('nav-list__item_active');
-  evt.currentTarget.classList.toggle('nav-list__item_active');
+  x.classList.toggle('nav-list__item_active');
+}));
+
+sliderControls.forEach(x => x.addEventListener('click', () => {
+  slides.forEach(x => x.classList.toggle('slide_active'))
+}));
+
+iphones.forEach(x => x.addEventListener('click', () => {
+  x.classList.toggle('iphone_disassembled');
+}));
+
+filterButtons.forEach(x => x.addEventListener('click', () => {
+  let currentActive = document.querySelector('.filter__button_active');
+
+  if (x !== currentActive) {
+    currentActive.classList.toggle('filter__button_active');
+    x.classList.toggle('filter__button_active');
+
+    let flexOrders = [];
+    for (let i = 0; i < galleryImages.length; i++) {
+      flexOrders.push(`${i}`);
+    }
+    flexOrders
+      .sort((a, b) => 0.5 - Math.random())
+      .sort((a, b) => Math.random() - Math.random())
+      .sort((a, b) => Math.random() - Math.random());
+
+    galleryImages.forEach((x, index) => x.style.order = flexOrders[index]);
+  }
+
 }));
 
