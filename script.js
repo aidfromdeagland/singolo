9874aaa54d4eaf180bf96038e1496cae63c872b3
aidfromdeagland <aidfromdeagland@gmail.com>
2020-03-15 13:44:15 +0300

    feat: implement form listeners and modal window logic

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+function shuffle(array) {
+  let m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
 }
 
-let navItems = document.querySelectorAll('.nav-list__item');
-let slider = document.querySelector('.slider');
-let sliderControls = slider.querySelectorAll('.slider-control');
-let slides = slider.querySelectorAll('.slide');
-let iphones = document.querySelectorAll('.iphone');
-let filterButtons = document.querySelectorAll('.filter__button');
-let galleryImages = document.querySelectorAll('.gallery__image');
+const navItems = document.querySelectorAll('.nav-list__item');
+const slider = document.querySelector('.slider');
+const sliderControls = slider.querySelectorAll('.slider-control');
+const slides = slider.querySelectorAll('.slide');
+const iphones = document.querySelectorAll('.iphone');
+const filterButtons = document.querySelectorAll('.filter__button');
+const galleryBlock = document.querySelector('.gallery');
+const galleryImages = document.querySelectorAll('.gallery__image');
+const feedbackForm = document.querySelector('.feedback');
+const modalForm = document.querySelector('.modal_form');
+
+const modalResolveButtonHandler = function () {
+  this.closest('.modal_active').classList.remove('modal_active');
+  feedbackForm.reset();
+  this.removeEventListener('click', modalResolveButtonHandler);
+};
+
+const modalOnEscapeHandler = function (evt) {
+  if (evt.code === 'Escape') {
+    if (modalForm.classList.contains('modal_active')) {
+      modalForm.classList.remove('modal_active');
+      feedbackForm.reset();
+      window.removeEventListener('keydown', modalOnEscapeHandler)
+    }
+  }
+};
 
 navItems.forEach(x => x.addEventListener('click', () => {
-  let currentActive = document.querySelector('.nav-list__item_active');
+  const currentActive = document.querySelector('.nav-list__item_active');
   currentActive.classList.toggle('nav-list__item_active');
   x.classList.toggle('nav-list__item_active');
 }));
 
 sliderControls.forEach(x => x.addEventListener('click', () => {
-  slides.forEach(x => x.classList.toggle('slide_active'))
+  slides.forEach(x => x.classList.toggle('slide_active'));
+  iphones.forEach(x => {
+    if (x.classList.contains('iphone_disassembled')) {
+      x.classList.remove('iphone_disassembled');
+    }
+  })
 }));
 
 iphones.forEach(x => x.addEventListener('click', () => {
@@ -25,7 +63,7 @@ iphones.forEach(x => x.addEventListener('click', () => {
 }));
 
 filterButtons.forEach(x => x.addEventListener('click', () => {
-  let currentActive = document.querySelector('.filter__button_active');
+  const currentActive = document.querySelector('.filter__button_active');
 
   if (x !== currentActive) {
     currentActive.classList.toggle('filter__button_active');
@@ -35,13 +73,42 @@ filterButtons.forEach(x => x.addEventListener('click', () => {
     for (let i = 0; i < galleryImages.length; i++) {
       flexOrders.push(`${i}`);
     }
-    flexOrders
-      .sort((a, b) => 0.5 - Math.random())
-      .sort((a, b) => Math.random() - Math.random())
-      .sort((a, b) => Math.random() - Math.random());
+    flexOrders = shuffle(flexOrders);
 
     galleryImages.forEach((x, index) => x.style.order = flexOrders[index]);
   }
-
 }));
+
+galleryImages.forEach(x => x.addEventListener('click', () => {
+  const currentSelected = galleryBlock.querySelector('.gallery__image_selected');
+  if (currentSelected) {
+    currentSelected.classList.toggle('gallery__image_selected');
+    x.classList.toggle('gallery__image_selected');
+  } else {
+    x.classList.toggle('gallery__image_selected');
+  }
+}));
+
+feedbackForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  if (!modalForm.classList.contains('modal_active')) {
+    const modalFormStatus = modalForm.querySelector('.form-modal__status');
+    const modalFormSubject = modalForm.querySelector('.form-modal__subject');
+    const modalFormDescription = modalForm.querySelector('.form-modal__description');
+
+    const subjectText = feedbackForm.querySelector('.feedback__input_subject').value;
+    const descriptionText = feedbackForm.querySelector('.feedback__textarea').value;
+
+    modalFormStatus.textContent = 'Письмо отправлено';
+    modalFormSubject.textContent = subjectText.trim() ? `Тема: ${subjectText.trim()}` : 'Без темы';
+    modalFormDescription.textContent = descriptionText.trim() ? `Описание: ${descriptionText.trim()}` : 'Без описания';
+
+    modalForm.classList.add('modal_active');
+    window.addEventListener('keydown', modalOnEscapeHandler);
+  }
+
+  const modalResolveButton = document.querySelector('.form-modal__button');
+  modalResolveButton.addEventListener('click', modalResolveButtonHandler);
+
+});
 
